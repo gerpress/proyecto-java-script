47c616ead0cd382f49fc3c2f2244ba26b168d5c2
German Presser <germanp_89@hotmail.com>
2023-02-09 20:09:54 -0300

    Pre entrega N 2 - German Presser

--- js/main.js
+++ js/main.js
@@ -1,166 +1,102 @@
-// let edad = parseInt(prompt("Ingrese su edad"));
-
-// while (edad <= 19) {
-//   alert("Este cálculo es solo para adultos");
-//   edad = parseInt(prompt("Ingrese una edad mayor o igual a 20"));
-// }
-// alert("Usted puede calcular su IMC");
-
-// const peso = parseFloat(prompt("Ingrese su peso"));
-// const altura = parseFloat(prompt("Ingrese su altura"));
-// const espacio = " ";
-
-// const IMC = peso / (altura * altura);
-// alert("Su IMC es:" + espacio + IMC);
-
-// if (IMC < 18.5) {
-//   alert("Se encuentra dentro del rango de peso insuficiente");
-// } else if (IMC > 18.5 && IMC < 24.9) {
-//   alert("se encuentra dentro del rango de peso normal o saludable");
-// } else if (IMC > 25.0 && IMC < 29.9) {
-//   alert("se encuentra dentro del rango de sobrepeso");
-// } else if (IMC >= 30) {
-//   alert("se encuentra dentro del rango de sobrepeso");
-// }
-
-// console.log("finaliza el programa");
-
 //CALCULO DE IMC
 
-// function Usuario(edad, peso, altura) {
-//   this.edad = edad;
-//   this.peso = peso;
-//   this.altura = altura;
-// }
+function Usuario(edad, peso, altura) {
+  this.edad = edad;
+  this.peso = peso;
+  this.altura = altura;
+}
 
-// let edad = parseInt(prompt("Ingrese una edad mayor o igual a 20"));
+let edad = parseInt(prompt("Ingrese una edad mayor o igual a 20"));
 
-// while (edad <= 19) {
-//   alert("Este cálculo es solo para adultos");
-//   edad = parseInt(prompt("Ingrese una edad mayor o igual a 20"));
-// }
-// alert("Usted puede calcular su IMC");
+while (edad <= 19) {
+  alert("Este cálculo es solo para adultos");
+  edad = parseInt(prompt("Ingrese una edad mayor o igual a 20"));
+}
+alert("Usted puede calcular su IMC");
 
-// const peso = parseInt(prompt("Ingrese su peso en kg"));
-// const altura = parseFloat(prompt("Ingrese su altura"));
-// const espacio = " ";
+const peso = parseInt(prompt("Ingrese su peso en kg"));
+const altura = parseFloat(prompt("Ingrese su altura"));
+const espacio = " ";
 
-// const IMC = peso / (altura * altura);
-// alert("Su IMC es:" + espacio + IMC);
+const IMC = peso / (altura * altura);
+alert("Su IMC es:" + espacio + IMC);
 
-// if (IMC < 18.5) {
-//   alert("Se encuentra dentro del rango de peso insuficiente");
-// } else if (IMC > 18.5 && IMC < 24.9) {
-//   alert("se encuentra dentro del rango de peso normal o saludable");
-// } else if (IMC > 25.0 && IMC < 29.9) {
-//   alert("se encuentra dentro del rango de sobrepeso");
-// } else if (IMC >= 30) {
-//   alert("se encuentra dentro del rango de sobrepeso");
-// }
+if (IMC < 18.5) {
+  alert("Se encuentra dentro del rango de peso insuficiente");
+} else if (IMC > 18.5 && IMC < 24.9) {
+  alert("se encuentra dentro del rango de peso normal o saludable");
+} else if (IMC > 25.0 && IMC < 29.9) {
+  alert("se encuentra dentro del rango de sobrepeso");
+} else if (IMC >= 30) {
+  alert("se encuentra dentro del rango de sobrepeso");
+}
 
-// const datosUsuario = new Usuario(edad, peso, altura);
+const datosUsuario = new Usuario(edad, peso, altura);
 
-// console.log(datosUsuario);
-// console.log("finaliza el programa");
+console.log(datosUsuario);
+console.log("finaliza el programa");
 
 //CLASES PRODUTOS
 
 class Producto {
-  constructor(nombre, precio, cantidad) {
+  constructor(nombre, precio) {
     this.nombre = nombre;
     this.precio = precio;
-    this.cantidad = cantidad;
     this.vendido = false;
-    this.subtotal = 0;
-  }
-
-  sumaEnvio() {
-    this.precio = this.precio + costoDelEnvio;
   }
 }
-
-const listaDeProductos = [new Producto("MUSCLE MAX X 90 TBS", 1820, 4), new Producto("ULTRA MASS X 1500 GRS", 4680, 4), new Producto("CREATINA MICRONIZADA X 300 GRS", 7500, 4), new Producto("PRE WAR (CON CAFEINA)", 3850, 4), new Producto("WHEY X PRO - 1 LB", 4320, 4), new Producto("WHEY PROTEIN TRUE MADE X 2.05 LB", 6500, 4)];
 
 //FUNCIONES
 
 function obtenerProducto(nombre) {
-  return listaDeProductos.find((Producto) => {
-    return Producto.nombre === nombre;
-  });
+  return listaDeProductos.find((producto) => producto.nombre === nombre);
 }
 
-function cantidadAComprar(cantidad) {
-  return listaDeProductos.find((cantidad) => {
-    return Producto.cantidad === cantidad;
-  });
-}
+const listaDeProductos = [new Producto("MUSCLE MAX X 90 TBS", 1820), new Producto("ULTRA MASS X 1500 GRS", 4680), new Producto("CREATINA MICRONIZADA X 300 GRS", 7500), new Producto("PRE WAR (CON CAFEINA)", 3850), new Producto("WHEY X PRO - 1 LB", 4320), new Producto("WHEY PROTEIN TRUE MADE X 2.05 LB", 6500)];
 
-function hayProducotsDisponibles(Producto, cantidadAComprar) {
-  const productosDisponibles = Producto.cantidad.filter((Producto) => {
-    return !Producto.vendido;
-  });
-
-  return productosDisponibles.length >= cantidadAComprar;
-}
-
-//COMPRA
-
-let productoAComprar = prompt("Ingrese el producto que desea comprar. Ingrese SALIR si quiere salir del programa");
+let productoAComprar = prompt("Ingrese el producto que desea comprar. Ingrese SALIR si no desea agregar mas productos");
 
 while (productoAComprar !== "SALIR") {
-  const Producto = obtenerProducto(productoAComprar);
+  let producto = obtenerProducto(productoAComprar);
 
-  //SOLICITO LA CANTIDAD
-  if (Producto !== undefined) {
-    const cantidad = (cantidadAComprar = parseInt(prompt("Ingrese la cantidad que desea comprar")));
+  console.log(producto);
 
-    while (cantidadAComprar <= 0 || !hayProducotsDisponibles(Producto, cantidadAComprar)) {
-      cantidadAComprar = parseInt(prompt("Ingrese la cantidad que desea comprar"));
+  let costoDelEnvio = 0;
+
+  if (producto !== undefined) {
+    //COSTO POR ZONA ENVIO
+
+    const zonaDeEnvio = prompt("Ingrese el lugar donde quiere recibir los productos para calcular el envio:");
+
+    switch (zonaDeEnvio) {
+      case "Ciudad de Buenos Aires":
+        costoDelEnvio = 1200;
+        break;
+
+      default:
+        costoDelEnvio = 3500;
+        break;
     }
 
-    console.log(cantidadAComprar);
-  } else {
-    alert("Producto no encontrado");
+    console.log("El costo del envio es: $" + costoDelEnvio);
   }
 
-  //SE LE VUELVE A PEDIR QUE INFRESE UN PRODUCTO
   productoAComprar = prompt("Ingrese el producto que desea comprar. Ingrese SALIR si quiere salir del programa");
+
+  let total = 0;
+  for (let producto of listaDeProductos) {
+    total += producto.precio + costoDelEnvio;
+  }
+
+  console.log("Total a pagar:" + total);
+
+  // subtotal();
+
+  // function subtotal() {
+  //   for (const producto of listaDeProductos) {
+  //     Producto.sumaEnvio();
+  //     Producto.subtotal = producto.precio + costoDelEnvio;
+  //     Producto.vendido = true;
+  //   }
+  // }
 }
-
-// //COSTO POR ZONA ENVIO
-
-// const zonaDeEnvio = prompt("Ingrese el lugar donde quiere recibir los productos para calcular el envio:");
-
-// let costoDelEnvio = 0;
-
-// switch (zonaDeEnvio) {
-//   case "Ciudad de Buenos Aires":
-//     costoDelEnvio = 1200;
-//     break;
-
-//   default:
-//     costoDelEnvio = 3500;
-//     break;
-// }
-
-// console.log("El costo del envio es: $" + costoDelEnvio);
-
-// //FUNCIONES
-
-// function obtenerProducto(cantidad) {
-//   return Productos.find((Producto) => {
-//     return Producto.cantidad === cantidad;
-//   });
-// }
-
-// function subtotal() {
-//   for (const Producto of Productos) {
-//     Producto.sumaEnvio();
-//     Producto.subtotal = Producto.precio + costoDelEnvio * Producto.cantidad;
-//     Producto.vendido = true;
-//   }
-// }
-
-// subtotal();
-
-// // const total = calcularTotalProducto(Producto, cantidadDeProductos);
